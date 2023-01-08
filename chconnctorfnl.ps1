70e26d56d40c7d5ae2db958047d39415ec519fc5
chabir abderrahim <101667634+chabir-ab@users.noreply.github.com>
2023-01-08 23:26:44 +0100

    Add files via upload

---
+++ chconnctorfnl.ps1
@@ -0,0 +1,36 @@
+﻿
+#bismilah hhh
+
+
+while ($true) {
+    
+    $pingres = Test-Connection -ComputerName 8.8.8.8 -Count 1 -Quiet
+    
+    if (!$pingres) {
+        
+        $interfaces = Get-NetAdapter | Where-Object {$_.status -eq "disconnected"}
+
+        if ($interfaces) {
+            
+            Enable-NetAdapter -name $interfaces.name
+            
+            Set-NetIPInterface -InterfaceIndex $interfaces.InterfaceIndex -Dhcp Enabled
+       
+            }
+        
+        ipconfig /release
+        ipconfig /renew
+            
+        }
+
+        else {
+            
+            Write-Output ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>---- You are Connected -----<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+            start-sleep -s 5
+            break
+        
+            }
+
+    }
+
+#ila khsrat lik xi haja matsnani n9adha lik dber rask hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
